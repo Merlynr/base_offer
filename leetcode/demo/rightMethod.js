9d3cbf6af431558ab41210a34e15d9eacd863f3c
Merlynr <lcq1013962426@gmail.com>
2022-07-20 18:10:09 +0800

    📦 比对器，冒泡

--- leetcode/demo/rightMethod.js
+++ leetcode/demo/rightMethod.js
@@ -1,21 +1,19 @@
 // right method
 function rightMethod(arr) {
-  Array.sort(function (a, b) {
-    return a - b;
-  });
+  if (Array.isArray(arr) && JSON.stringify(arr) !== "[]") {
+    arr.sort((a, b) => {
+      return a - b;
+    });
+  }
 }
 // random Array
 function generateRandomArray(maxSize, maxValue) {
   let random_arr = new Array(Math.floor((maxSize + 1) * Math.random()));
-  for (let i in random_arr.length) {
+  for (let i = 0; i < random_arr.length; i++) {
     random_arr[i] = Math.floor(Math.random() * (maxValue + 1));
   }
   return random_arr;
 }
-console.log(
-  "🚀 ~ file: rightMethod.js ~ line 15 ~ generateRandomArray ~ random_arr",
-  generateRandomArray(5, 4)
-);
 
 // 硬拷贝数组
 function copyArr(arr) {
@@ -36,6 +34,73 @@ function isEqual(arr1, arr2) {
     return true;
   }
   for (let i in arr1) {
-    return arr[i];
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function Test() {
+  let testTimes = 500000;
+  let maxSize = 99;
+  let maxValue = 99;
+  let succeed = true;
+  console.log("🚀 ~ file: rightMethod.js ~ ~ Test ~ Starting");
+  let sum_t1 = 0.0,
+    sum_t2 = 0.0;
+  while (testTimes > 0) {
+    let arr = generateRandomArray(maxSize, maxValue);
+    if (arr.length == 0) {
+      continue;
+    }
+    let arr1 = copyArr(arr);
+    let arr2 = copyArr(arr);
+    // ! 函数比较
+    let t1 = performance.now();
+    rightMethod(arr1);
+    let t1_e = performance.now();
+    sum_t1 += t1_e - t1;
+
+    var t2 = performance.now();
+    bubbleSort(arr2);
+    var t2_e = performance.now();
+    sum_t2 += t2_e - t2;
+
+    if (!isEqual(arr1, arr2)) {
+      succeed = false;
+      console.log(arr1, arr2);
+      break;
+    }
+    --testTimes;
+  }
+  console.log("🚀 ~ file: rightMethod.js ~ ~ Test ~ Starting");
+  console.log(
+    succeed
+      ? `🌞${testTimes} epochs ：Bubble spent time ${sum_t1}ms\n
+      Array.sort() spent time ${sum_t2}ms\n`
+      : "Error"
+  );
+}
+
+// 冒泡排序
+function bubbleSort(arr) {
+  if (Array.isArray(arr) && JSON.stringify(arr) !== "[]") {
+    for (let end = arr.length - 1; end > 0; end--) {
+      for (let i = 0; i < end; i++) {
+        if (arr[i] > arr[i + 1]) {
+          swap(arr, i, i + 1);
+        }
+      }
+    }
   }
 }
+
+// 交换
+function swap(array, left, right) {
+  array[left] ^= array[right];
+  array[right] ^= array[left];
+  array[left] ^= array[right];
+}
+
+Test();
--- leetcode/demo/test_algorithm.js
+++ leetcode/demo/test_algorithm.js
@@ -1,110 +1,3 @@
-<<<<<<< .mine
-// 待测试的
-function insertSort(arr) {
-  for (const [i, value] of Object.entries(arr)) {
-    for (let j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
-      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-    }
-  }
-}
-
-// 简单方法
-function rightMethod(arr) {
-  arr.sort((a, b) => a - b);
-}
-
-//随机数生成器
-function generateRandomArray(maxSize, maxValue) {
-  // 生成长度，值随机的数组
-  let random_arr = new Array(Math.floor((maxSize + 1) * Math.random()));
-  for (let i = 0; i < random_arr.length; i++) {
-    random_arr[i] = Math.floor((maxValue + 1) * Math.random());
-  }
-  return random_arr;
-}
-
-// 拷贝数组
-function copyArr(arr) {
-  if (arr == null) {
-    return null;
-  }
-  return [].concat(arr);
-}
-
-function isEqual(arr1, arr2) {
-  if (arr1 ^ arr2) {
-    return false;
-  }
-  if (!arr1 & !arr2) {
-    return true;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i in arr1) {
-    if (arr1[i] != arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-var quickSort_1 = function (array) {
-  if (array.length <= 1) {
-    return array;
-  }
-  // 选择基准
-  var pivotIndex = Math.floor(array.length >> 1);
-  //   在原数组中删除pivot同时返回pivot
-  var pivot = array.splice(pivotIndex, 1)[0];
-  var left = [];
-  var right = [];
-
-  for (const ele of array) {
-    if (ele < pivot) {
-      left.push(ele);
-    } else {
-      right.push(ele);
-    }
-  }
-  //   递归
-  return quickSort_1(left).concat(pivot, quickSort_1(right));
-};
-
-// test
-const qs_1 = require("./quick_sort");
-const quickSort_2 = require("./quick_sort");
-const quickSort_3 = require("./quick_sort");
-
-function Test() {
-  let testTimes = 500;
-  let maxSize = 12;
-  let maxValue = 12;
-  let succeed = true;
-  console.log("starting----");
-  while (testTimes > 0) {
-    let arr = generateRandomArray(maxSize, maxValue);
-    let arr1 = copyArr(arr);
-    let arr2 = copyArr(arr);
-    // insertSort(arr1);
-    console.log(arr1);
-
-    quickSort_2.quickSort_2(arr1);
-    rightMethod(arr2);
-    if (!isEqual(arr1, arr2)) {
-      succeed = false;
-      console.log(arr1);
-      break;
-    }
-    console.log(testTimes);
-    --testTimes;
-  }
-  console.log(succeed ? "succeed" : "Error");
-}
-
-Test();
-// console.log(quickSort_1([3,1,4,1,5,9,2,6]));
-=======
 // 待测试的
 function insertSort(arr) {
   for (const [i, value] of Object.entries(arr)) {
@@ -194,15 +87,15 @@ function Test() {
     let arr2 = copyArr(arr);
     // insertSort(arr1);
     console.log(arr1);
-    
-    quickSort_2.quickSort_2(arr1)
+
+    quickSort_2.quickSort_2(arr1);
     rightMethod(arr2);
     if (!isEqual(arr1, arr2)) {
       succeed = false;
       console.log(arr1);
       break;
     }
-    console.log(testTimes)
+    console.log(testTimes);
     --testTimes;
   }
   console.log(succeed ? "succeed" : "Error");
@@ -210,4 +103,3 @@ function Test() {
 
 Test();
 // console.log(quickSort_1([3,1,4,1,5,9,2,6]));
->>>>>>> .theirs
