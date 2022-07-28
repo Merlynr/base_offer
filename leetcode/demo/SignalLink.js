3beefc0c3a062f8645d6ead89ea277a7c3cad93e
merlynr <lcq1013962426@gmail.com>
2022-07-28 19:45:28 +0800

    Changes to be committed: 	modified:   leetcode/demo/SignalLink.js

--- leetcode/demo/SignalLink.js
+++ leetcode/demo/SignalLink.js
@@ -2,7 +2,7 @@
  * @Author: Merlynr
  * @Date: 2022-07-24 17:47:39
  * @Last Modified by: Merlynr
- * @Last Modified time: 2022-07-26 00:39:07
+ * @Last Modified time: 2022-07-28 19:45:01
  */
 
 class Node {
