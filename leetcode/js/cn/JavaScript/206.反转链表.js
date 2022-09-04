3345bc89495809fd47134693bf484760c465710e
merlynr <lcq1013962426@gmail.com>
2022-09-05 00:33:29 +0800

    🐛

---
+++ leetcode/js/cn/JavaScript/206.反转链表.js
@@ -0,0 +1,22 @@
+// @algorithm @lc id=206 lang=javascript 
+// @title reverse-linked-list
+import * as a from 'algm'
+import { ListNode } from 'algm'
+// @author Merlynr
+// @test([1,2,3,4,5])=[5,4,3,2,1]
+// @test([1,2])=[2,1]
+// @test([])=[]
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+};
